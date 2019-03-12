75a3691ced242634166eb7ae2144a863bed3bf5a
ballsbs2 <ballsbs2@gmail.com>
2019-03-12 11:15:44 -0400

    Added multithreading
    added multithreading and made the code more effecient

--- Data Transport/app/routes.py
+++ Data Transport/app/routes.py
@@ -5,15 +5,14 @@ from flask import request, jsonify
 @app.route('/send', methods=['POST'])
 def send():
 
-#   getting the json data
+    app.logger.info("test")
+
     sensors = request.json
 
-#   parsing the json data
     for sensor in sensors:
         name = sensor["name"]
         data = sensor["data"]
 
-#       writing the json data to the file
         try:
             with open(name + ".txt", "a") as file:
 
@@ -24,9 +23,7 @@ def send():
                 app.logger.info("closing: " + name + ".txt")
 
         except IOError as e:
-#           error writnig data to file
             app.logger.info("Couldn't open or write to file (%s)." % e)
-#           returns 500 error if cannot write data
             abort(500)
 
 
