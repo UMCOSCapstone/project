850f8015547d289e0eb77b4c6666c7eba5f7ee8d
Chi Anh Nguyen <37124810+chianguyen@users.noreply.github.com>
2019-04-03 11:27:35 -0500

    Update Graph.js

--- Data Visualization/Program/my-app/src/components/Graph.js
+++ Data Visualization/Program/my-app/src/components/Graph.js
@@ -22,7 +22,7 @@ class Graph extends Component {
 componentDidMount() {
     const socket = socketIOClient("http://localhost:5000/test");
     socket.on("newnumber", data => this.addDataPoint(data.number));
-    socket.on("newnum", data2 => this.addDataPoint(data2.number1));
+//    socket.on("newnum", data2 => this.addDataPoint(data2.number1));
   }
 
 
@@ -84,7 +84,7 @@ componentDidMount() {
 
           <YAxis>
             <YAxis.Title>Y-axis</YAxis.Title>
-            <LineSeries name="Channel 2" data={this.state.data2}/>
+ //           <LineSeries name="Channel 2" data={this.state.data2}/>
           </YAxis>
         </HighchartsChart>
       </div>
